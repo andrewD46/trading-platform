8be5e6eb4f944899f997a95f70c4e36d2c35dc5a
AndrewD46 <den46den46@gmail.com>
2020-10-15 12:11:47 +0300

    Models added

---
+++ app/models.py
@@ -0,0 +1,110 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class StockBase(models.Model):
+    """Base"""
+    code = models.CharField("Code", max_length=8, unique=True)
+    name = models.CharField("Name", max_length=128, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class Currency(StockBase):
+    """Currency"""
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = "Currency"
+        verbose_name_plural = "Currencies"
+
+
+class Item(StockBase):
+    """Particular stock"""
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    currency = models.ForeignKey(Currency, blank=True, null=True, on_delete=models.SET_NULL)
+    details = models.TextField("Details", blank=True, null=True, max_length=512)
+
+
+class WatchList(models.Model):
+    """Current user, favorite list of stocks"""
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+
+
+class Price(models.Model):
+    """Item price"""
+    currency = models.ForeignKey(Currency, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.CASCADE, related_name='price',
+                             related_query_name='prices', )
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    date = models.DateTimeField(unique=True, blank=True, null=True, )
+
+
+class Offer(models.Model):
+    """Request to buy or sell specific stocks"""
+
+    ORDER_TYPE = [
+        (0, 'Created'),
+        (1, 'Inprocess'),
+        (2, 'Done'),
+    ]
+
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+    entry_quantity = models.IntegerField("Requested quantity")
+    quantity = models.IntegerField("Current quantity")
+    order_type = models.PositiveSmallIntegerField(choices=ORDER_TYPE)
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Inventory(models.Model):
+    """The number of stocks a particular user has"""
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+    quantity = models.IntegerField("Stocks quantity", default=0)
+
+
+class Trade(models.Model):
+    """Information about a certain transaction"""
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+    seller = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='seller_trade',
+        related_query_name='seller_trade',
+    )
+    buyer = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='buyer_trade',
+        related_query_name='buyer_trade',
+    )
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    buyer_offer = models.ForeignKey(
+        Offer,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='buyer_trade',
+        related_query_name='buyer_trade',
+    )
+    seller_offer = models.ForeignKey(
+        Offer,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='seller_trade',
+        related_query_name='seller_trade',
+    )
