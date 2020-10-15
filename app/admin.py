0f0f83bfae92eeeaa5bfb4618b778178c15f084e
Andrew <den46den46@gmail.com>
2020-10-15 15:58:05 +0300

    Revert "Add Serializers"

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-from .models import Currency, Item, WatchList, Price, Offer, Inventory, Trade
-
-admin.site.register(Currency)
-admin.site.register(Item)
-admin.site.register(WatchList)
-admin.site.register(Price)
-admin.site.register(Offer)
-admin.site.register(Inventory)
-admin.site.register(Trade)
+# Register your models here.
